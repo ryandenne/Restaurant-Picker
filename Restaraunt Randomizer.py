a158f46d4f005c01fe47a0c4fe364de8dfad99fb
Ryan Denne <87154429+ryandenne@users.noreply.github.com>
2024-11-11 09:41:25 -0600

    Update Restaraunt Randomizer.py
    added table lookup for restaurant list

--- Restaraunt Randomizer.py
+++ Restaraunt Randomizer.py
@@ -1,12 +1,23 @@
 import tkinter as tk
 import random
+import pandas as pd
 
-def add_restaurant():
+def load_restaurants(): # Manage excel list of restaurants
+    df = pd.read_excel(r'C:\Users\ryanj\OneDrive\Documents\Python Scripts\Restaurants.xlsx')
+    return df['Restaurants'].tolist()
+
+def add_restaurant(): # Add restaurant manually/temporarily
     restaurant = entry.get()
     if restaurant:
         restaurants.append(restaurant)
         entry.delete(0, tk.END)
         update_listbox()
+
+def randomize_restaurant(): #Randomizer FUnction
+    if restaurants:
+        chosen = random.choice(restaurants)
+        result_label.config(text=f"How about {chosen}", font=("Helvitica", 16, "bold"), bg="yellow")
+        flash_label(6) # Flash Count
 
 def flash_label(count):
     if count > 0:
@@ -14,12 +25,6 @@ def flash_label(count):
         new_color = "yellow" if current_color == root.cget('bg') else root.cget('bg')
         result_label.config(bg=new_color)
         root.after(500, flash_label, count - 1) # Flash effect
-
-def randomize_restaurant():
-    if restaurants:
-        chosen = random.choice(restaurants)
-        result_label.config(text=f"How about {chosen}", font=("Helvitica", 16, "bold"), bg="yellow")
-        flash_label(6) # Flash Count
 
 def update_listbox():
     listbox.delete(0, tk.END)
@@ -32,7 +37,7 @@ root.title("Where to Eat?")
 root.geometry("300x500")
 
 # Initialize restaurant list
-restaurants = ["Anejo", "Sake House", "BRGR", "Long Horns", "Burg & Barrel"]
+restaurants = load_restaurants()
 
 # Listbox to display restaurants
 listbox = tk.Listbox(root)
